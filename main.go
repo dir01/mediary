7de93990ab16309f85b115ec2eb46816d57a641a
Andrew Zhu <dir01@dir01.org>
2022-08-02 22:53:22 +0300

    gh action to deploy the app

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "hello, fam\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
