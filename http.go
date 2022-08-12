8f7cac64fdc2ee45c6201187e983df07b78062b3
Andrew Zhu <dir01@dir01.org>
2022-08-12 22:55:09 +0300

    implemented tests and docs for /metadata endpoint

--- http.go
+++ http.go
@@ -3,14 +3,15 @@ package mediary
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"time"
 )
 
 func PrepareHTTPServerMux(service *Service) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/metadata", handleMetadata(service, 5*time.Second))
-	mux.HandleFunc("/metadata/long-polling", handleMetadata(service, 0))
+	mux.HandleFunc("/metadata", handleMetadata(service, 100*time.Millisecond))
+	mux.HandleFunc("/metadata/long-polling", handleMetadata(service, 5*time.Minute))
 	return mux
 }
 
@@ -25,6 +26,8 @@ func handleMetadata(svc *Service, timeout time.Duration) func(http.ResponseWrite
 
 		if metadata, err := svc.GetMetadata(ctx, req.URL.Query().Get("url")); err == nil {
 			respondWithJSON(w, http.StatusOK, metadata)
+		} else if errors.Is(err, context.DeadlineExceeded) {
+			respondWithJSON(w, http.StatusAccepted, `{"foo": "bar"}`)
 		} else {
 			respondWithJSON(w, http.StatusInternalServerError, err.Error())
 		}
@@ -32,11 +35,20 @@ func handleMetadata(svc *Service, timeout time.Duration) func(http.ResponseWrite
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
+	var response []byte
+	switch payload := payload.(type) {
+	case error:
+		response = []byte(payload.Error())
+	case string:
+		response = []byte(payload)
+	default:
+		var err error
+		response, err = json.MarshalIndent(payload, "", "  ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
 	}
 	w.WriteHeader(code)
 	w.Write(response)
