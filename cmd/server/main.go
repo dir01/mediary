623b81a975e833c0c0e2ae292cbf41a02fa58a11
Andrew Zhu <dir01@dir01.org>
2023-01-22 01:07:20 +0200

    REDIS_URL_BG_JOBS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/dir01/mediary/config"
 	"github.com/dir01/mediary/downloader"
 	"github.com/dir01/mediary/downloader/torrent"
 	mediary_http "github.com/dir01/mediary/http"
@@ -21,12 +22,20 @@ import (
 
 func main() {
 	_ = godotenv.Load()
-	conf := config.New()
+
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalf("error initializing logger: %v", err)
 	}
 	defer func() { _ = logger.Sync() }()
+
+	mustGetEnv := func(key string) string {
+		value, ok := os.LookupEnv(key)
+		if !ok {
+			logger.Fatal("missing env var", zap.String("key", key))
+		}
+		return value
+	}
 
 	// torrentDownloader downloads torrents
 	torrentDownloader, err := torrent.NewTorrentDownloader(os.TempDir(), logger)
@@ -37,12 +46,34 @@ func main() {
 	// dwn is a composite downloader: it can download anything, as long as one of its minions knows how to
 	dwn := downloader.NewDownloader([]service.Downloader{torrentDownloader})
 
-	// redisClient will be used both for storage and queue, mostly because I've found some cloud redis with a free tier
-	opt, _ := redis.ParseURL(conf.MustGetRedisURL())
-	redisClient := redis.NewClient(opt)
-	defer func() { _ = redisClient.Close() }()
+	mkRedisClient := func(url string) (client *redis.Client, teardown func()) {
+		opt, err := redis.ParseURL(url)
+		if err != nil {
+			logger.Fatal("error parsing redis url", zap.Error(err))
+		}
+		redisClient := redis.NewClient(opt)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if _, err := redisClient.Ping(ctx).Result(); err != nil {
+			logger.Fatal("error connecting to redis", zap.Error(err))
+		}
+		return redisClient, func() { _ = redisClient.Close() }
+	}
 
-	queue, err := jobsqueue.NewRedisJobsQueue(redisClient, 2, "mediary", logger)
+	// redisClient will be used both for storage
+	redisURL := mustGetEnv("REDIS_URL")
+	redisClient, teardownRedis := mkRedisClient(redisURL)
+	defer teardownRedis()
+
+	// and for jobs queue
+	bgRedisURL := os.Getenv("REDIS_URL_BG_JOBS")
+	if bgRedisURL == "" {
+		bgRedisURL = redisURL
+	}
+	bgRedisClient, teardownBgRedis := mkRedisClient(bgRedisURL)
+	defer teardownBgRedis()
+
+	queue, err := jobsqueue.NewRedisJobsQueue(bgRedisClient, 2, "mediary", logger)
 	if err != nil {
 		log.Fatalf("error initializing redis jobs queue: %v", err)
 	}
