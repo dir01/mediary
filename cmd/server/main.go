73f999cb7938a3fa48a64a7ed8e76d9276cd656e
Andrew Zhu <dir01@dir01.org>
2023-04-14 21:04:07 +0300

    redis initial connection timeout 5s -> 20s

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -79,7 +79,7 @@ func main() {
 			logger.Fatal("error parsing redis url", zap.Error(err))
 		}
 		redisClient := redis.NewClient(opt)
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 		defer cancel()
 		if _, err := redisClient.Ping(ctx).Result(); err != nil {
 			logger.Fatal("error connecting to redis", zaperr.ToField(err))
